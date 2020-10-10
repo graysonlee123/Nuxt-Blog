850e0555461125a79ecb8220deaae9924f089775
Grayson Gantek <graysonleegantek@gmail.com>
2020-10-10 00:40:37 -0400

    Extends Feature and Teaser components

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,6 +1,10 @@
 <template>
-  <div v-editable="blok" class="py-2">
+  <div v-editable="blok" class="py-2 text-center">
+    <img class="mx-auto" :src="blok.icon.filename" alt="Icon" />
     <h1 class="text-lg">{{ blok.name }}</h1>
+    <p class="text-gray-600">
+      {{ blok.description }}
+    </p>
   </div>
 </template>
 
