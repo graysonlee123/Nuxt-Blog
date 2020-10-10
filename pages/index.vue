b0bcc0ad8c75f5719540698dbd6a41c683963517
Grayson Gantek <graysonleegantek@gmail.com>
2020-10-10 01:41:00 -0400

    Now displays featured articles on home page

--- pages/index.vue
+++ pages/index.vue
@@ -57,5 +57,16 @@ export default {
         }
       });
   },
+  async fetch(context) {
+    // Loading reference data - Articles in our case
+    if (context.store.state.articles.loaded !== "1") {
+      let articlesRefRes = await context.app.$storyapi.get(`cdn/stories/`, {
+        starts_with: "articles/",
+        version: "draft",
+      });
+      context.store.commit("articles/setArticles", articlesRefRes.data.stories);
+      context.store.commit("articles/setLoaded", "1");
+    }
+  },
 };
 </script>
